8044353deb427263375e76879173e835ecc9b083
Jose Maria <103214579+josxmag16@users.noreply.github.com>
2023-07-12 21:21:47 +0200

    Update autoNmap.sh
    añadidas modificaciones en el nmap, función para detener el programa con CTRL + C y función que explica el uso del programa

--- autoNmap.sh
+++ autoNmap.sh
@@ -1,7 +1,22 @@
 #!/bin/bash
 
+# Verificar que se haya proporcionado un argumento de IP
+if [ $# -ne 1 ]; then
+  echo "Uso: $0 <ip a escanear>"
+  exit 1
+fi
+
+# Función para manejar la señal de interrupción (Ctrl + C)
+function handle_interrupt() {
+  echo "[*] Saliendo del programa..."
+  exit 0
+}
+
+# Asignar la función handle_interrupt al manejo de la señal SIGINT
+trap handle_interrupt SIGINT
+
 # Ejecutar el primer comando de Nmap para obtener los puertos abiertos
-nmap -p- --open --min-rate 5000 -n -Pn "$1" -oG allPorts > /dev/null 2>&1
+nmap -p- -sS --open --min-rate 5000 -vvv -n -Pn "$1" -oG allPorts > /dev/null 2>&1
 
 # Guardo los puertos abiertos para pasarselos a la siguiente funcion
 ports="$(cat allPorts | grep -oP '\d{1,5}/open' | awk '{print $1}' FS='/' | xargs | tr ' ' ',')"
@@ -15,4 +30,5 @@ tail -n +4 targeted | head -n -3 >> nmapResolution
 
 # Muestro el archivo con colores usando batcat
 batcat nmapResolution -l java
+
 
